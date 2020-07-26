07017f944be77d0e68c54e4ef2fe9b068f08518b
Matias <maticapuano97@gmail.com>
2020-07-25 22:58:13 -0500

    Added & implements all routes, components.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import React from "react";
 
-const App = () => {
-  return <div className="App">Hello word</div>;
-};
+import { ModalProvider, BaseModalBackground } from "styled-react-modal";
+import styled from "styled-components";
 
-export default App;
+import Routes from "./routes";
+
+import AuthContextProvider from "./store/context/AuthContext";
+import FeedContextProvider from "./store/context/FeedContext";
+import FollowContextProvider from "./store/context/FollowContext";
+import UploadContextProvider from "./store/context/UploadContext";
+import SearchContextProvider from "./store/context/SearchContext";
+import ProfileContextProvider from "./store/context/ProfileContext";
+import PostContextProvider from "./store/context/PostContext";
+
+import GlobalStyle from "./styles/GlobalStyle";
+
+const FadingBackground = styled(BaseModalBackground)`
+  opacity: ${(props) => props.opacity};
+  transition: opacity ease 200ms;
+`;
+
+export default () => {
+  return (
+    <AuthContextProvider>
+      <FeedContextProvider>
+        <FollowContextProvider>
+          <UploadContextProvider>
+            <SearchContextProvider>
+              <ProfileContextProvider>
+                <PostContextProvider>
+                  <GlobalStyle />
+                  <ModalProvider backgroundComponent={FadingBackground}>
+                    <Routes />
+                  </ModalProvider>
+                </PostContextProvider>
+              </ProfileContextProvider>
+            </SearchContextProvider>
+          </UploadContextProvider>
+        </FollowContextProvider>
+      </FeedContextProvider>
+    </AuthContextProvider>
+  );
+};
